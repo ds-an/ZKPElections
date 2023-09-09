6e3eabf594982f515c935c4b44dd4c30cb7c8da9
ds-an <ds-an@tuta.io>
2023-09-09 22:25:29 +0300

    Semi-final.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 
-candidates = ("Democrat", "Republican")
+candidates = ("Демократ", "Республиканец")
 
 
 def generate_key_pair_rsa():
@@ -46,8 +46,9 @@ class Voter:
         for center in centers_list:
             encrypted_vote = encrypt_rsa(center.public_key, self.vote_preference)
             zkp_hash_signature = create_zkp_signature(self.private_key, encrypted_vote)
-            signed_encrypted_vote = (encrypted_vote, zkp_hash_signature[1])
+            signed_encrypted_vote = (encrypted_vote, zkp_hash_signature, self.public_key)
             center.collected_votes.append(signed_encrypted_vote)
+
 
 class Center:
     def __init__(self):
@@ -63,17 +64,19 @@ class Center:
 
     def tally_votes(self):
         for vote in self.collected_votes:
-            if verify_zkp(self.public_key, vote[0], vote[1]):
+            voter_public_key = vote[2]
+            encrypted_vote_hashed = vote[1][0]
+            signature_zkp = vote[1][1]
+            if verify_zkp(voter_public_key, encrypted_vote_hashed, signature_zkp):
                 self.valid_votes += 1
                 if decrypt_rsa(self.private_key, vote[0]) == candidates[0]:
                     self.votes_democrat += 1
                 elif decrypt_rsa(self.private_key, vote[0]) == candidates[1]:
                     self.votes_republican += 1
-                break
             else:
                 self.invalid_votes += 1
-        print(f"Согласно центру подсчета голосов №{self.number_of_center}:\n")
-        print(f"Общее количество голосов составляет: {self.valid_votes}\n")
+        print(f"Согласно центру подсчета голосов №{self.number_of_center}:")
+        print(f"Общее количество голосов составляет: {self.valid_votes}")
         print(f"Количество выявленных невалидных голосов: {self.invalid_votes}")
         print(f"Количество голосов, поданных за кандидата от Демократической партии: {self.votes_democrat}")
         print(f"Количество голосов, поданных за кандидата от Республиканской партии: {self.votes_republican}")
@@ -97,17 +100,17 @@ def create_zkp_signature(voter_private_key, encrypted_vote):
     encrypted_vote_hashed = hashlib.sha256(encrypted_vote).digest()
     signature_zkp = voter_private_key.sign(
         encrypted_vote_hashed,
-        padding.PSS(padding.MGF1(hashes.SHA256),
+        padding.PSS(padding.MGF1(hashes.SHA256()),
                     padding.PSS.MAX_LENGTH),
         hashes.SHA256()
     )
     return encrypted_vote_hashed, signature_zkp
 
 
-def verify_zkp(center_public_key, encrypted_vote, signature_zkp):
+def verify_zkp(voter_public_key, encrypted_vote_hashed, signature_zkp):
     try:
-        center_public_key.verify(
-            signature_zkp, encrypted_vote, padding.PSS(
+        voter_public_key.verify(
+            signature_zkp, encrypted_vote_hashed, padding.PSS(
                 padding.MGF1(hashes.SHA256()),
                 padding.PSS.MAX_LENGTH
             ),
@@ -117,34 +120,133 @@ def verify_zkp(center_public_key, encrypted_vote, signature_zkp):
     except Exception as exception:
         return False
 
-# def vote_secure(voter, centers_list):
-#     for center in centers_list:
-#         encrypted_vote = encrypt_rsa(center.public_key, voter.vote_preference)
-#         zkp_hash_signature = create_zkp_signature(voter.private_key, encrypted_vote)
-#         signed_encrypted_vote = (encrypted_vote, zkp_hash_signature[1])
-#         center.collected_votes.append(signed_encrypted_vote)
+
+def verify_third_party(centers_list):
+    valid_votes = 0
+    invalid_votes = 0
+    center = centers_list[0]
+    for vote in center.collected_votes:
+        voter_public_key = vote[2]
+        encrypted_vote_hashed = vote[1][0]
+        signature_zkp = vote[1][1]
+        if verify_zkp(voter_public_key, encrypted_vote_hashed, signature_zkp):
+            valid_votes += 1
+        else:
+            invalid_votes += 1
+    if valid_votes == center.valid_votes:
+        print("Голоса были верифицированы третьей стороной и выборы признаны состоявшимися.")
+    else:
+        print("Третьей стороне не удалось верифицировать голоса и выборы признаны не состоявшимися.")
 
 
-def verify_third_party():
-    pass
-
-
-def voter_registration():
-    pass
-
-
-def begin_elections():
-    voters_list = generate_voters(20)
-    centers_list = generate_centers(3)
+def voter_registration(voters_list):
+    registered_voters = []
     for voter in voters_list:
-        voter.vote_secure(centers_list)
-    # print(voters_list[19].private_key)
-    # print(voters_list[19].public_key)
-    # message = "I'm a message."
-    # encrypted_message = encrypt_rsa(voters_list[19].public_key, message)
-    # decrypted_message = decrypt_rsa(voters_list[19].private_key, encrypted_message)
-    # print(f"{decrypted_message}")
-    print(time.process_time())
+        if voter.id in range(311111111, 399999999):
+            # Булев параметр здесь - статус проголосовал/не проголосовал
+            registered_voters.append([voter.id, voter.public_key, False])
+        else:
+            print(f"Голосующий №{voter.id} имеет невалидный id"
+                  f" и был выявлен при попытке регистрации.")
+    return registered_voters
 
 
-begin_elections()
+def check_voter_registration(voter, registered_voters_list):
+    for voter_id_key_and_status in registered_voters_list:
+        if voter.id == voter_id_key_and_status[0] and \
+                voter_id_key_and_status[2] is False:
+            voter_id_key_and_status[2] = True
+            return True
+        elif voter.id == voter_id_key_and_status[0] and \
+                voter_id_key_and_status[2] is True:
+            print(f"Голосующий №{voter.id} пытался проголосовать дважды и был выявлен.")
+        else:
+            pass
+    return False
+
+
+def begin_elections_scenario_1():
+    number_voters = int(input("Введите количество голосущих: "))
+    number_centers = int(input("Введите количество центров подсчета голосов: "))
+    print("Генерируем голосующих...")
+    voters_list = generate_voters(number_voters)
+    print("Генерируем центры подсчета голосов...")
+    centers_list = generate_centers(number_centers)
+    print("Проводим регистрацию голосующих...")
+    registered_voters_list = voter_registration(voters_list)
+    # voters_list.append(Voter())
+    print("Проводим голование...")
+    for voter in voters_list:
+        if check_voter_registration(voter, registered_voters_list):
+            voter.vote_secure(centers_list)
+        else:
+            print(f"The voter {voter.id} not on the list")
+    for center in centers_list:
+        center.tally_votes()
+    print("Проводим верификацию голосования стейкхолдером...")
+    verify_third_party(centers_list)
+
+
+def begin_elections_scenario_2():
+    number_voters = int(input("Введите количество голосущих: "))
+    number_centers = int(input("Введите количество центров подсчета голосов: "))
+    print("Генерируем голосующих...")
+    voters_list = generate_voters(number_voters)
+    malicious_voter = voters_list[random.randint(1, number_voters)]
+    malicious_voter.id = 234343434 # невалидный id
+    malicious_voter.malicious = True
+    print("Генерируем центры подсчета голосов...")
+    centers_list = generate_centers(number_centers)
+    print("Проводим регистрацию голосующих...")
+    registered_voters_list = voter_registration(voters_list)
+    # voters_list.append(Voter())
+    print("Проводим голование...")
+    for voter in voters_list:
+        if check_voter_registration(voter, registered_voters_list):
+            voter.vote_secure(centers_list)
+        else:
+            print(f"Голосующий №{voter.id} не зарегистрирован и не будет допущен к голосованию.")
+    for center in centers_list:
+        center.tally_votes()
+    print("Проводим верификацию голосования стейкхолдером...")
+    verify_third_party(centers_list)
+
+
+def begin_elections_scenario_3():
+    number_voters = int(input("Введите количество голосущих: "))
+    number_centers = int(input("Введите количество центров подсчета голосов: "))
+    print("Генерируем голосующих...")
+    voters_list = generate_voters(number_voters)
+    malicious_voter = voters_list[random.randint(1, number_voters)]
+    malicious_voter.malicious = True
+    print("Генерируем центры подсчета голосов...")
+    centers_list = generate_centers(number_centers)
+    print("Проводим регистрацию голосующих...")
+    registered_voters_list = voter_registration(voters_list)
+    # voters_list.append(Voter())
+    print("Проводим голование...")
+    for voter in voters_list:
+        if check_voter_registration(voter, registered_voters_list):
+            voter.vote_secure(centers_list)
+        else:
+            print(f"Голосующий №{voter.id} не зарегистрирован и не будет допущен к голосованию.")
+    if check_voter_registration(malicious_voter, registered_voters_list):
+        malicious_voter.vote_secure()
+    for center in centers_list:
+        center.tally_votes()
+    print("Проводим верификацию голосования стейкхолдером...")
+    verify_third_party(centers_list)
+
+
+scenario = int(input("Выберите сценарий голосования:\n"
+                     "1. Сценарий без эксцессов\n"
+                     "2. Сценарий с попыткой проголосовать без валидного id\n"
+                     "3. Cценарий с попыткой проголосовать дважды\n"))
+if scenario == 1:
+    begin_elections_scenario_1()
+elif scenario == 2:
+    begin_elections_scenario_2()
+elif scenario == 3:
+    begin_elections_scenario_3()
+else:
+    print("Некорректный вариант сценация голосования: введите 1, 2 или 3.")
